ca091cd12c869094ec907e2072fb19571de1f9cb
Zachary Hubbard <zhubbard190@gmail.com>
2022-06-13 16:52:13 -0400

    Added comments to PlayerMovement.cs

--- Dawn of Alina/Assets/Scenes/Zach's Test Scenes/PlayerMovement.cs
+++ Dawn of Alina/Assets/Scenes/Zach's Test Scenes/PlayerMovement.cs
@@ -4,38 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 12f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 2f;
-
+    // Variables for player movement
+    public float speed = 12f; // Speed of player
+    public float gravity = -9.81f; // Gravity
+    public float jumpHeight = 2f; // Height of Jump
+    Vector3 velocity; // velocity vector
+    
     public CharacterController controller;
     public Transform groundCheck;
-    public float groundDistance = 0.2f;
-    public LayerMask groundmask;
-    Vector3 velocity;
-    bool isGrounded;
+    public float groundDistance = 0.2f; // Distance from ground
+    public LayerMask groundmask; // Layer for ground
+    bool isGrounded; // Check if player is on the ground
 
-    // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundmask);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundmask); // Checks if ground object is within range of player
 
-        if (isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0) // removes velocity if grounded
         {
-            velocity.y = -2f;
+            velocity.y = -2f; 
         }
 
-        float x = Input.GetAxis("Horizontal");
+        float x = Input.GetAxis("Horizontal"); // Movement Axes
         float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = transform.right * x + transform.forward * z; // chooses move vector from where player is facing
+        controller.Move(move * speed * Time.deltaTime); // Moves player
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        // Player Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) // Jumps player
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); // Calculates height of jump
         }
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime; // Sets velocity in Y direction using gravity (falling)
+        controller.Move(velocity * Time.deltaTime); 
     }
 }
