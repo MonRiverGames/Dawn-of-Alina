a807bbae7f2019a515ffa2b4b587c8495a23fe3d
Zachary Hubbard <zhubbard190@gmail.com>
2022-06-14 00:07:49 -0400

    Updated PlayerCamSwitch.cs
    Updated PlayerCamSwitch.cs

--- Dawn of Alina/Assets/Scenes/Zach's Test Scenes/PlayerCamSwitch.cs
+++ Dawn of Alina/Assets/Scenes/Zach's Test Scenes/PlayerCamSwitch.cs
@@ -6,44 +6,24 @@ public class PlayerCamSwitch : MonoBehaviour
 {
     public GameObject FirstCam; // First-person camera
     public GameObject ThirdCam; // Third-person camera
-    public int camMode; // Specifies either 1st or 3rd Person
+    bool isThirdPerson = false; // Boolean specifies either 1st or 3rd Person
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.C)) // Switches cam mode on letter press
         {
-            if (camMode == 1)
-            {
-                camMode = 0;
-            }
-
-            else
-            {
-                camMode += 1;
-            }
-
-            StartCoroutine(CamChange()); // Change Camera
+            isThirdPerson = !isThirdPerson;
+            StartCoroutine(CameraChange()); // Change Camera
         }
     }
 
-    IEnumerator CamChange() // Sets the active camera and enables appropriate audio listener
+    IEnumerator CameraChange() // Sets the active camera and enables appropriate audio listener
     {
         yield return new WaitForSeconds(0.01f);
-
-        if(camMode == 0) // First Person
-        {
-            FirstCam.SetActive(true);
-            ThirdCam.SetActive(false);
-            FirstCam.GetComponent<AudioListener>().enabled = true;
-            ThirdCam.GetComponent<AudioListener>().enabled = false;
-        }
-
-        if (camMode == 1) // Third Person
-        {
-            ThirdCam.SetActive(true);
-            FirstCam.SetActive(false);
-            FirstCam.GetComponent<AudioListener>().enabled = false;
-            ThirdCam.GetComponent<AudioListener>().enabled = true;
-        }
+        FirstCam.SetActive(!isThirdPerson);
+        ThirdCam.SetActive(isThirdPerson);
+        FirstCam.GetComponent<AudioListener>().enabled = !isThirdPerson; 
+        ThirdCam.GetComponent<AudioListener>().enabled = isThirdPerson;
+        yield return null;
     }
 }
