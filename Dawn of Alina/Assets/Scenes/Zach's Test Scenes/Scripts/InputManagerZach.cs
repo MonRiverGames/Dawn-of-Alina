15ef3260c5bde1e1c5ab7dc2e35e1be8f49b17c0
Zachary Hubbard <zhubbard190@gmail.com>
2022-06-15 17:13:36 -0400

    Added Camera Switching to work with new input manager in test scene

---
+++ Dawn of Alina/Assets/Scenes/Zach's Test Scenes/Scripts/InputManagerZach.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManagerZach : MonoBehaviour
+{
+    private ZachPlayerInput playerInput;
+    public ZachPlayerInput.OnFootActions onFoot;
+    public ZachPlayerInput.CameraControlActions cameraControl;
+    private PlayerMotor playerMotor;
+    private PlayerLookZach playerLook;
+    
+    void Awake()
+    {
+        playerInput = new ZachPlayerInput();
+        onFoot = playerInput.OnFoot;
+        cameraControl = playerInput.CameraControl;
+        
+        playerMotor = GetComponent<PlayerMotor>();
+        playerLook = GetComponent<PlayerLookZach>();
+        
+        onFoot.Jump.performed += ctx => playerMotor.Jump();
+        onFoot.Crouch.performed += ctx => playerMotor.Crouch();
+        onFoot.Sprint.performed += ctx => playerMotor.Sprint();
+        cameraControl.SwitchCameraView.performed += ctx => playerLook.getCamChange();
+    }
+
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        //Tell playermotor to move using value from movement action
+        playerMotor.ProcessMove(onFoot.Movement.ReadValue<Vector3>());
+    }
+
+    private void LateUpdate()
+    {
+        playerLook.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+        cameraControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+        cameraControl.Disable();
+    }
+}
