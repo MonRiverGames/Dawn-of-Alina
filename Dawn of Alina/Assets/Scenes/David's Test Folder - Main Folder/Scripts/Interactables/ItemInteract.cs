30b5edd55c79f53709a1ede06a084cb223fd91b6
Zachary Hubbard <zhubbard190@gmail.com>
2022-07-12 12:04:20 -0400

    Player can now pickup item as an interactable

---
+++ Dawn of Alina/Assets/Scenes/David's Test Folder - Main Folder/Scripts/Interactables/ItemInteract.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteract : Interactable
+{
+    public Item item;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void Interact()
+    {
+        InventoryManager.instance.AddItem(item);
+        Destroy(gameObject);
+    }
+
+}
